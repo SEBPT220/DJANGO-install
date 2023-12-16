f744097a4e873616f5d133a7f76ab2e948061a22
Denis S Dujota <denisdujota@gmail.com>
2023-12-16 17:50:13 -0500

    add sopngs model

--- tunr_django/tunr/models.py
+++ tunr_django/tunr/models.py
@@ -12,6 +12,7 @@ class Song(models.Model):
   title = models.CharField(max_length=100)
   album = models.CharField(max_length=100)
   preview_url = models.TextField()
+  artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name="songs", default=1)
 
   def __str__(self):
     return f"#{self.title} on albmum: #{self.album}"
