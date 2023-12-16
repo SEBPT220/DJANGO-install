a89e636841606119ad1b7ebe98db4908493680c0
Denis S Dujota <denisdujota@gmail.com>
2023-12-16 16:28:46 -0500

    add song model

--- tunr_django/tunr/models.py
+++ tunr_django/tunr/models.py
@@ -7,3 +7,11 @@ class Artist(models.Model):
 
   def __str__(self):
     return self.name
+
+class Song(models.Model):
+  title = models.CharField(max_length=100)
+  album = models.CharField(max_length=100)
+  preview_url = models.TextField()
+
+  def __str__(self):
+    return f"#{self.title} on albmum: #{self.album}"
